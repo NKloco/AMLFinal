99818754053440b38f00735b707eb6b6e19f4978
Nitzan Katz <nitzkk@gmail.com>
2021-08-29 02:53:38 +0300

    Added DTCR fake sample creation functionality

---
+++ Related Works/DTCR/DTCR.py
@@ -0,0 +1,42 @@
+"""
+This module will provide the functionality required to recreate the results of the deep temporal clustering representation(DTCR) algorithm.
+"""
+import math
+import random
+
+FAKE_SAMPLE_ALPHA = 0.2 # As set in the article
+
+def create_fake_time_series(sample, fake_alpha=FAKE_SAMPLE_ALPHA):
+    fake_sample = list(sample)
+    time_steps_to_shuffle = math.floor(len(sample) * fake_alpha)
+    indices_to_shuffle = random.sample(range(len(sample)), time_steps_to_shuffle)
+    while len(indices_to_shuffle) > 1:
+        # Choosing the indices to shuffle (The last one with a random one)
+        last_index = indices_to_shuffle.pop() # Decrease the length by 1
+        random_index_to_swap = indices_to_shuffle[random.randint(0, len(indices_to_shuffle) - 1)]
+
+        # Swapping the items
+        swap_temp = fake_sample[last_index]
+        fake_sample[last_index] = fake_sample[random_index_to_swap]
+        fake_sample[random_index_to_swap] = swap_temp
+    
+    return fake_sample
+
+def main():
+    print("Testing the DTCR functionality...")
+    # Creating a sample with 1000 values to check the fake sampling functionality.
+    sample = range(1000)
+    fake_sample = create_fake_time_series(sample)
+
+    shuffled_count = 0
+
+    for index in range(len(sample)):
+        if sample[index] != fake_sample[index]:
+            shuffled_count += 1
+
+    print("For a sample of length {}, there were {} shuffles, which is {} of the series.".format(len(sample), shuffled_count, shuffled_count/len(sample)))
+
+    print("DTCR functionality finished testing.")
+
+if __name__ == "__main__":
+    main()
