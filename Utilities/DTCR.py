b2c942bed8c74884310cab37dec9abc61c24014c
Nitzan Katz <nitzkk@gmail.com>
2021-08-31 23:52:37 +0300

    Added pylint formatting to the code files

--- Utilities/DTCR.py
+++ Utilities/DTCR.py
@@ -1,10 +1,12 @@
 """
-This module will provide the functionality required to recreate the results of the deep temporal clustering representation(DTCR) algorithm.
+This module will provide the functionality required to recreate the results of
+the deep temporal clustering representation(DTCR) algorithm.
 """
 import math
 import random
 
-FAKE_SAMPLE_ALPHA = 0.2 # As set in the article
+FAKE_SAMPLE_ALPHA = 0.2  # As set in the article
+
 
 def create_fake_time_series(sample, fake_alpha=FAKE_SAMPLE_ALPHA):
     fake_sample = list(sample)
@@ -13,23 +15,27 @@ def create_fake_time_series(sample, fake_alpha=FAKE_SAMPLE_ALPHA):
     time_steps_to_shuffle = math.floor(len(sample) * fake_alpha)
 
     # The indices to shuffle
-    indices_to_shuffle = random.sample(range(len(sample)), time_steps_to_shuffle)
-    
+    indices_to_shuffle = random.sample(range(len(sample)),
+                                       time_steps_to_shuffle)
+
     while len(indices_to_shuffle) > 1:
         # Choosing the indices to shuffle (The last one with a random one)
-        last_index = indices_to_shuffle.pop() # Decrease the length by 1
-        random_index_to_swap = indices_to_shuffle[random.randint(0, len(indices_to_shuffle) - 1)]
+        last_index = indices_to_shuffle.pop()  # Decrease the length by 1
+        random_index_to_swap = indices_to_shuffle[
+            random.randint(0, len(indices_to_shuffle) - 1)]
 
         # Swapping the items
         swap_temp = fake_sample[last_index]
         fake_sample[last_index] = fake_sample[random_index_to_swap]
         fake_sample[random_index_to_swap] = swap_temp
-    
+
     return fake_sample
+
 
 def main():
     print("Testing the DTCR functionality...")
-    # Creating a sample with 1000 values to check the fake sampling functionality.
+    # Creating a sample with 1000 values to check the fake
+    # sampling functionality.
     sample = range(1000)
     fake_sample = create_fake_time_series(sample)
 
@@ -39,9 +45,12 @@ def main():
         if sample[index] != fake_sample[index]:
             shuffled_count += 1
 
-    print("For a sample of length {}, there were {} shuffles, which is {} of the series.".format(len(sample), shuffled_count, shuffled_count/len(sample)))
+    print("For a sample of length {}, there were {} shuffles".format(
+        len(sample), shuffled_count))
+    print("Which is {} of the series.".format(shuffled_count/len(sample)))
 
     print("DTCR functionality finished testing.")
+
 
 if __name__ == "__main__":
     main()
--- Utilities/UCRParser.py
+++ Utilities/UCRParser.py
@@ -1,12 +1,16 @@
 """
-This is a utility class made to convert the UCR datasets into pytorch dataset classes.
-The use of this module should be importing the read_dataset function which receives the wanted dataset name and resturns a train and a test pytorch dataset.
+This is a utility class made to convert the UCR
+datasets into pytorch dataset classes.
+The use of this module should be importing the read_dataset function which
+receives the wanted dataset name and resturns a train and a test pytorch
+dataset.
 """
 
 import os
 from torch.utils.data import Dataset
 
 UCR_DATASETS_PATH = "UCRDatasets"
+
 
 class UCRDataset(Dataset):
     def __init__(self, data):
@@ -17,10 +21,9 @@ class UCRDataset(Dataset):
         for time_sample in data:
             if time_sample.label not in labels:
                 labels.apppend(time_sample.label)
-        
+
         self._labels = labels
         self._num_of_labels = len(self._labels)
-
 
     def __getitem__(self, idx):
         sample = self._data[idx]
@@ -28,36 +31,40 @@ class UCRDataset(Dataset):
 
     def __len__(self):
         return self._len
-    
+
     @property
     def number_of_labels(self):
         return self._num_of_labels
+
 
 class Sample(object):
     def __init__(self, sample_text):
         data = sample_text.split(",")
         self._label = int(data[0])
         self._sample_data = [float(sample) for sample in data[1:]]
-    
+
     @property
     def data(self):
         return self._sample_data
-    
+
     @property
     def label(self):
         return self._label
 
+
 def get_available_datasets():
     return os.listdir(UCR_DATASETS_PATH)
+
 
 def get_single_dataset(path):
     with open(path, "r") as data_file:
         data = data_file.readlines()
-    
+
     data_samples = [Sample(sample) for sample in data]
     dataset = UCRDataset(data_samples)
 
     return dataset
+
 
 def read_dataset(dataset_name):
     print("Loading the {} dataset...".format(dataset_name))
@@ -72,16 +79,19 @@ def read_dataset(dataset_name):
 
     print("The dataset {} was loaded.".format(dataset_name))
     return train_dataset, test_dataset
-    
+
 
 def main():
     print("Testing the UCRParser utility module...")
     train, test = read_dataset("ArrowHead")
-    print("The dataset has {} training samples and {} test samples.".format(len(train), len(test)))
+    print("The dataset has {} training samples and {} test samples.".format(
+        len(train), len(test)))
     print("The first sample from the training set is {}".format(train[0]))
     available_datasets = get_available_datasets()
-    print("There are {} available UCR datasets. Which are {}".format(len(available_datasets), ",".join(available_datasets)))
+    print("There are {} available UCR datasets. Which are {}".format(
+        len(available_datasets), ",".join(available_datasets)))
     print("UCRParser module tested successfully!")
+
 
 if __name__ == "__main__":
     main()
