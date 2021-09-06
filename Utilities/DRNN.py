16b6b3bcb43ad7a3143a49ac644428e0b74dec83
Nitzan Katz <nitzkk@gmail.com>
2021-09-06 09:49:38 +0300

    Fixed output of bidirectional to fit inputs order

--- Utilities/DRNN.py
+++ Utilities/DRNN.py
@@ -189,8 +189,12 @@ class BidirectionalDRNN(nn.Module):
         backwards_outputs, backwards_hidden = \
             self._backwards_drnn.forward(reversed_inputs, backwards_hidden)
 
+        # We want the outputs to concatenate the same sample's outputs
+        # so we need to reverse the outputs of the backwards network
+        backwards_outputs_reversed = self._reverse_inputs(backwards_outputs)
+
         combined_outputs = torch.cat(
-            (regular_outputs, backwards_outputs), dim=2)
+            (regular_outputs, backwards_outputs_reversed), dim=2)
 
         return combined_outputs, regular_hidden + backwards_hidden
 
